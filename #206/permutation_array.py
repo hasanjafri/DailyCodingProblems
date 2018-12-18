3690b1c5e7e70635d0e222507913ca449d6e8431
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-18 17:25:13 -0500

    Optimize solution for #206 to be one liner

--- #206/permutation_array.py
+++ #206/permutation_array.py
@@ -1,9 +1,5 @@
 def permutate_array(lst, perm):
-    permutated_array = []
-    for num in perm:
-        permutated_array.append(lst[num])
-
-    return permutated_array
+    return [lst[num] for num in perm]
 
 if __name__ == "__main__":
     print(permutate_array(["a", "b", "c"], [2, 1, 0]))
