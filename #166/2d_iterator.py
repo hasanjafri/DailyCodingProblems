5d5a1a3f0feb593181b47f5859659931dddfdf45
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-19 20:41:47 -0500

    Potential working solution, WIP: improve next() function

---
+++ #166/2d_iterator.py
@@ -0,0 +1,36 @@
+class tdIterator:
+    def __init__(self, arrayList):
+        self.arrayList = arrayList
+
+    def next(self):
+        try:
+            if isinstance(self.arrayList[0], list):
+                if len(self.arrayList[0]) > 0:
+                    num = self.arrayList[0][0]
+                    self.arrayList[0].remove(num)
+                    if len(self.arrayList[0]) == 0:
+                        self.arrayList.remove(self.arrayList[0])
+                    return num
+                else:
+                    self.arrayList.remove(self.arrayList[0])
+                    self.next()
+        except IndexError:
+            raise
+
+    
+    def has_next(self):
+        try:
+            if isinstance(self.arrayList[0], list):
+                if len(self.arrayList[0]) > 0:
+                    return True
+                else:
+                    return False
+        except IndexError:
+            raise
+
+if __name__ == "__main__":
+    td_iterator = tdIterator([[1, 2], [3], [], [4, 5, 6]])
+    import pdb; pdb.set_trace()
+    for y in range(7):
+        print(td_iterator.next())
+    
