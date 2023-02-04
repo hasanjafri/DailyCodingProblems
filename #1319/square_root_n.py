eacfb21fc10d832e0b563ae322c895f9ee275cee
Hasan <hasan.jafri7@gmail.com>
2023-02-04 16:02:38 -0500

    #1319: Find square root of N

---
+++ #1319/square_root_n.py
@@ -0,0 +1,10 @@
+import math
+
+
+def find_square_root(n):
+    return math.sqrt(n)
+
+
+if __name__ == "__main__":
+    print(find_square_root(4))  # Output: 2.0
+    print(find_square_root(9))  # Output: 3.0
